e5328d8b67fadd8d0ac3736e3bd2b9600a4ab5b8
Laila Suter <e227401@edu.sbl.ch>
2024-11-22 07:20:41 +0000

    Aufgabe 21

--- script.js
+++ script.js
@@ -324,3 +324,17 @@ export function aufgabe20(args) {
   return true
 }
 linkupExerciseHandler('[data-click="aufgabe20"]', aufgabe20)
+
+export function aufgabe21(args) {
+  const input = args
+  const result = []
+  // Speichere die ganze Eingabe
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    result.push(currentElement)
+  }
+  // Nimm die ganze Eingabe und kehre sie um
+  return result.reverse().join("")
+}
+linkupExerciseHandler('[data-click="aufgabe21"]', aufgabe21)
